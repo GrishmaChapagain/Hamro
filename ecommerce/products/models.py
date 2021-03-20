cac73d2d9cacae02bda41e99092b2c535edf938b
GrishmaChapagain <58723487+GrishmaChapagain@users.noreply.github.com>
2021-03-20 22:41:57 +0545

    Customer models

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+PRADESH_CHOICE = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4 ', '4'),
+    ('5 ', '5'),
+    ('6 ', '6'),
+    ('7 ', '7'),
+)
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    place = models.CharField(max_length=200)
+    area = models.CharField(max_length=200)
+    Pradesh = models.CharField(choices=PRADESH_CHOICE, max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+
+
+
---
+++ ecommerce/products/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+
+
+
+urlpatterns = [
+    path('', views.home),
+  ,
+    
+
+ 
+
+    
+] 
