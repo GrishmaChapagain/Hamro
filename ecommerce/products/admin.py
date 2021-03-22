35d0698ccc9995dfa1e6dc34de7a6d80b625cd71
GrishmaChapagain <58723487+GrishmaChapagain@users.noreply.github.com>
2021-03-22 16:12:43 +0545

    added models

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Customer
+from .models import (Customer,Product,Cart,OrderPlaced)
 
 
-admin.site.register(Customer)
-# class CustomerModelAdmin(admin.ModelAdmin):
-#     list_display=['id','user','name','place','area','Pradesh']
+
+@ admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','name','place','area','Pradesh']
+
+
+@ admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display=['id','title','Price','Selling_Price','Description','Manufacturer','category','Image']
+
+# @ admin.register(Cart)
+# class CartModelAdmin(admin.ModelAdmin):
+#     list_display=['id','user','product','quantity']
+
+# @ admin.register(OrderPlaced)
+# class OrderPlacedModelAdmin(admin.ModelAdmin):
+#     list_display=['id','user','customer','product','quantity','Purchased_Date','Status']
--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 
@@ -23,6 +24,59 @@ class Customer(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+
+PASAL_CHOICES = (
+
+    ('M', 'MAKEUP'),
+    ('F', 'FASHION_WEARS'),
+    ('E', 'Electronics'),
+    ('FO', 'FOODS'),   
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    Price = models.FloatField()
+    Selling_Price= models.FloatField()
+    Description = models.TextField()
+    Manufacturer = models.CharField(max_length=100)
+    category = models.CharField(choices=PASAL_CHOICES, max_length=2)
+    Image = models.ImageField(upload_to='image')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+
+
+
+STATUS_CHOICES = (
+    ('A', 'Accepted'),
+    ('P', 'Packed'),
+    ('O', 'On The Way'),
+    ('D', 'Delievered'),
+    ('C', 'Cancel')
+)
+
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    Purchased_Date = models.DateTimeField(auto_now_add=True)
+    Status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+
 
 
 
