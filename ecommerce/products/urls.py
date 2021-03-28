f0bafb0dbe7285943e9554532d393b834bef31e0
GrishmaChapagain <58723487+GrishmaChapagain@users.noreply.github.com>
2021-03-28 16:38:26 +0545

    Login and registration completed

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -3,11 +3,14 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 
 
 urlpatterns = [
     path('', views.home),
+    path('login/', auth_views.LoginView.as_view(template_name='pr/login.html',authentication_form=LoginForm), name='login'),
+    path('registration/', views.CustomerRegistrationView.as_view(),name='customerregistration'),
   
        
 ] 
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
+
 
 
 
@@ -9,8 +12,6 @@ def add_to_cart(request):
 def checkout(request):
  return render(request, 'pr/checkout.html')
 
-def customerregistration(request):
- return render(request, 'pr/customerregistration.html')
 
 def home(request):
  return render(request, 'pr/home.html')
@@ -22,4 +23,35 @@ def product_detail(request):
  return render(request, 'pr/productdetail.html')
 
 
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'pr/customerregistration.html', {'form': form})
 
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Congratulations, Your Account has been registered.')
+            form.save()
+        return render(request, 'pr/customerregistration.html', {'form': form, 'active': 'btn-primary'})
+
+
+
+def loginVew(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'pr/login.html', context)
